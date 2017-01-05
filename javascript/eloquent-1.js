e90afbcfc7a03598855fbe2a1cfce4b252d2afad
Roger Souza <roger.souza@owinteractive.com>
2017-01-05 16:44:11 -0200

    Primeiro exercício do eloquent Javascript
    http://braziljs.github.io/eloquente-javascript/chapters/estrutura-do-pro
    grama/

--- javascript/eloquent-1.js
+++ javascript/eloquent-1.js
@@ -1,38 +1,60 @@
 /*jslint sloppy:true, white:true, nomen:true, browser:true */
 /*global $, console, alert */
 
-var tree = '#', i = 1;
+    var tree = '#', i = 1;
 
-for (i = 1; i <= 7; i = i + 1) {
+    for (i = 1; i <= 7; i = i + 1) {
 
-    if (i === 1) {
+        if (i === 1) {
 
-        tree = tree;
+            tree = tree;
 
-    } else {
+        } else {
 
-        tree += '#';
+            tree += '#';
+
+        }
+
+        console.log(i + ' ' + tree);
 
     }
 
-    console.log(i + ' ' + tree);
+    tree = '';
 
-}
+    for (i = 1; i <= 7; i = i + 1) {
 
-tree = '';
+        tree += '#';
 
-for (i = 1; i <= 7; i = i + 1) {
+        console.log(i + ' ' + tree);
 
-    tree += '#';
+    }
 
-    console.log(i + ' ' + tree);
+    tree = '#';
 
-}
+    for (i = 1; i <= 7; i = i + 1) {
 
-tree = '#';
+        console.log(i + ' ' + tree.repeat(i));
 
-for (i = 1; i <= 7; i = i + 1) {
+    }
 
-    console.log(i + ' ' + tree.repeat(i));
+    tree = '#', i = 1;
 
-}
+    do {
+
+        console.log(i + ' ' + tree);
+
+        i = i + 1;
+        tree += '#';
+
+    } while (i <= 7);
+
+    tree = '#', i = 1;
+
+    while (i <= 7) {
+
+        console.log(i + ' ' + tree);
+
+        tree += '#';
+        i     = i + 1;
+
+    }
