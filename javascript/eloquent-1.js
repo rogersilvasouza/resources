913ba436186d87bbc1e35345e07548f168f385b4
Roger Souza <roger.souza@owinteractive.com>
2017-01-05 16:54:22 -0200

    Primeiro exercício do eloquent Javascript
    http://braziljs.github.io/eloquente-javascript/chapters/estrutura-do-pro
    grama/

--- javascript/eloquent-1.js
+++ javascript/eloquent-1.js
@@ -1,6 +1,16 @@
 /*jslint sloppy:true, white:true, nomen:true, browser:true */
 /*global $, console, alert */
 
+    // 1 Modo
+    console.log('#');
+    console.log('##');
+    console.log('###');
+    console.log('####');
+    console.log('#####');
+    console.log('######');
+    console.log('#######');
+
+    // 2 Modo
     var tree = '#', i = 1;
 
     for (i = 1; i <= 7; i = i + 1) {
@@ -15,46 +25,52 @@
 
         }
 
-        console.log(i + ' ' + tree);
+        console.log(tree);
 
     }
 
+    // 3 Modo
     tree = '';
 
     for (i = 1; i <= 7; i = i + 1) {
 
         tree += '#';
 
-        console.log(i + ' ' + tree);
+        console.log(tree);
 
     }
 
+    // 4 Modo
     tree = '#';
 
     for (i = 1; i <= 7; i = i + 1) {
 
-        console.log(i + ' ' + tree.repeat(i));
+        console.log(tree.repeat(i));
 
     }
 
+    // 5 Modo
     tree = '#', i = 1;
 
     do {
 
-        console.log(i + ' ' + tree);
+        console.log(tree);
 
         i = i + 1;
         tree += '#';
 
     } while (i <= 7);
 
+    // 6 Modo
     tree = '#', i = 1;
 
     while (i <= 7) {
 
-        console.log(i + ' ' + tree);
+        console.log(tree);
 
         tree += '#';
         i     = i + 1;
 
     }
+
+    // 7 Modo
