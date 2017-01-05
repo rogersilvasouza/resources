c9417c8d3d2a01b7a4dd088ca6bf0e4386196849
Roger Silva Souza <rogersilvasouza@hotmail.com>
2017-01-05 21:10:40 -0200

    Primeiro exercício do eloquent Javascript
    for (i = 1; i <= 7; i = i + 1) {
    
            tree += '#';
    
            console.log(tree);
    
        }

--- javascript/eloquent-1.js
+++ javascript/eloquent-1.js
@@ -1,7 +1,20 @@
 /*jslint sloppy:true, white:true, nomen:true, browser:true */
 /*global $, console, alert */
 
+    // Primeiro exercício do eloquent Javascript
+    var tree = '#', i = 1;
+
+    // Best way
+    for (i = 1; i <= 7; i = i + 1) {
+
+        tree += '#';
+
+        console.log(tree);
+
+    }
+
     // 1 Modo
+    console.time('wayOfDoing1');
     console.log('#');
     console.log('##');
     console.log('###');
@@ -9,17 +22,15 @@
     console.log('#####');
     console.log('######');
     console.log('#######');
+    console.timeEnd('wayOfDoing1');
 
     // 2 Modo
-    var tree = '#', i = 1;
+    console.time('wayOfDoing2');
+    tree = '#'; i = 1;
 
     for (i = 1; i <= 7; i = i + 1) {
 
-        if (i === 1) {
-
-            tree = tree;
-
-        } else {
+        if (i !== 1) {
 
             tree += '#';
 
@@ -28,8 +39,10 @@
         console.log(tree);
 
     }
+    console.timeEnd('wayOfDoing2');
 
     // 3 Modo
+    console.time('wayOfDoing3');
     tree = '';
 
     for (i = 1; i <= 7; i = i + 1) {
@@ -39,8 +52,10 @@
         console.log(tree);
 
     }
+    console.timeEnd('wayOfDoing3');
 
     // 4 Modo
+    console.time('wayOfDoing4');
     tree = '#';
 
     for (i = 1; i <= 7; i = i + 1) {
@@ -48,9 +63,11 @@
         console.log(tree.repeat(i));
 
     }
+    console.timeEnd('wayOfDoing4');
 
     // 5 Modo
-    tree = '#', i = 1;
+    console.time('wayOfDoing5');
+    tree = '#'; i = 1;
 
     do {
 
@@ -60,9 +77,11 @@
         tree += '#';
 
     } while (i <= 7);
+    console.timeEnd('wayOfDoing5');
 
     // 6 Modo
-    tree = '#', i = 1;
+    console.time('wayOfDoing6');
+    tree = '#'; i = 1;
 
     while (i <= 7) {
 
@@ -72,8 +91,10 @@
         tree += '#';
 
     }
+    console.timeEnd('wayOfDoing6');
 
     // 7 Modo
+    console.time('wayOfDoing7');
     for (i = 1; i <= 7; i = i + 1) {
 
         switch (i <= 7) {
@@ -113,5 +134,4 @@
         }
 
     }
-
-    // 8 Modo
+    console.timeEnd('wayOfDoing7');
