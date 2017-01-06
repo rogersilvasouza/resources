292208bd911e868212b45d1c5e1cfc0f81a11ea0
Roger Souza <roger.souza@owinteractive.com>
2017-01-06 12:53:05 -0200

    Exercicio 3 eloquent
    Exercicio 3 eloquent

--- javascript/eloquent-2-fizzbuzz-mod-resto.js
+++ javascript/eloquent-2-fizzbuzz-mod-resto.js
@@ -1,18 +1,19 @@
 /*jslint sloppy:true, white:true, nomen:true, browser:true */
 /*global $, console, alert */
 
-    // 1 Modo
+    // 1 Modo 9.59ms
+    console.time('wayOfDoing1');
     var i = 1;
 
     for (var i = 1; i <= 100; i = i + 1) {
 
         if ((i % 3) === 0) {
 
-            console.log('Fizz');
+            console.log('FizzBuzz');
 
         } else if ((i % 5) === 0) {
 
-            console.log('Buzz');
+            console.log('FizzBuzz');
 
         } else {
 
@@ -21,3 +22,73 @@
         }
 
     }
+    console.timeEnd('wayOfDoing1');
+
+    // 2 Modo 11ms
+    console.time('wayOfDoing2');
+    i = 1;
+
+    while (i <= 100) {
+
+      if ((i % 3) === 0) {
+
+          console.log('FizzBuzz');
+
+      } else if ((i % 5) === 0) {
+
+          console.log('FizzBuzz');
+
+      } else {
+
+          console.log(i);
+
+      }
+
+      i = i + 1;
+
+    }
+    console.timeEnd('wayOfDoing2');
+
+    // 3 Modo 9.65ms
+    console.time('wayOfDoing3');
+    i = 1;
+
+    do {
+
+        if ((i % 3) === 0) {
+
+            console.log('FizzBuzz');
+
+        } else if ((i % 5) === 0) {
+
+            console.log('FizzBuzz');
+
+        } else {
+
+            console.log(i);
+
+        }
+
+        i = i + 1;
+
+    } while (i <= 100);
+    console.timeEnd('wayOfDoing3');
+
+    // 4 Modo 8.89ms
+    console.time('wayOfDoing4');
+    var i = 1;
+
+    for (var i = 1; i <= 100; i = i + 1) {
+
+        if (((i % 3) === 0) || ((i % 5) === 0) ) {
+
+            console.log('FizzBuzz');
+
+        } else  {
+
+            console.log('FizzBuzz');
+
+        }
+
+    }
+    console.timeEnd('wayOfDoing4');
---
+++ javascript/eloquent-3-tabuleiro-de-xadrez.js
@@ -0,0 +1,49 @@
+/*jslint sloppy:true, white:true, nomen:true, browser:true */
+/*global $, console, alert */
+
+    // 1 Modo
+    console.time('wayOfDoing1');
+    var i = 1, j = 1, board = '';
+
+    for (i = 1; i <= 8; i = i + 1) {
+
+        if ((i % 2) === 1) {
+
+            for (j = 1; j <= 8; j = j + 1) {
+
+                if (((j % 2) === 1)) {
+
+                    board += '#';
+
+                } else {
+
+                    board += ' ';
+
+                }
+
+            }
+
+        } else {
+
+            for (j = 1; j <= 8; j = j + 1) {
+
+                if (((j % 2) === 1)) {
+
+                    board += ' ';
+
+                } else {
+
+                    board += '#';
+
+                }
+
+            }
+
+        }
+
+        board += '\n';
+
+    }
+
+    console.log(board);
+    console.timeEnd('wayOfDoing1');
