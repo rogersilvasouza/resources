34b86318eabf86173745f03bef1b130b7e7bc02e
Roger Souza <roger.souza@owinteractive.com>
2017-01-09 13:01:09 -0200

    Terceiro exercício Tabuleiro de Xadrez
    Terceiro exercício Tabuleiro de Xadrez

--- javascript/eloquent-3-tabuleiro-de-xadrez.js
+++ javascript/eloquent-3-tabuleiro-de-xadrez.js
@@ -1,5 +1,5 @@
 /*jslint sloppy:true, white:true, nomen:true, browser:true */
-/*global $, console, alert */
+/*global $, console, alert, prompt */
 
     // 1 Modo
     console.time('wayOfDoing1');
@@ -50,47 +50,51 @@
 
     // 2 Modo
     console.time('wayOfDoing2');
-    var i = 1, j = 1, board = '';
+    var numberOfLines = prompt('Número de linhas ?'), numberOfRows = prompt('Número de colunas ?'), i = 1, j = 1, board = '';
 
-    for (i = 1; i <= 8; i = i + 1) {
+    if (numberOfLines && numberOfRows) {
 
-        if ((i % 2) === 1) {
+      for (i = 1; i <= numberOfLines; i = i + 1) {
 
-            for (j = 1; j <= 8; j = j + 1) {
+          if ((i % 2) === 1) {
 
-                if (((j % 2) === 1)) {
+              for (j = 1; j <= numberOfRows; j = j + 1) {
 
-                    board += '#';
+                  if (((j % 2) === 1)) {
 
-                } else {
+                      if ((i % 2) === 1) {
 
-                    board += ' ';
+                          board += '#';
 
-                }
+                      } else {
 
-            }
+                          board += ' ';
 
-        } else {
+                      }
 
-            for (j = 1; j <= 8; j = j + 1) {
+                  } else {
 
-                if (((j % 2) === 1)) {
+                      if ((i % 2) === 1) {
 
-                    board += ' ';
+                          board += ' ';
 
-                } else {
+                      } else {
 
-                    board += '#';
+                          board += '#';
 
-                }
+                      }
 
-            }
+                  }
 
-        }
+              }
 
-        board += '\n';
+          }
+
+          board += '\n';
+
+      }
+
+      console.log(board);
 
     }
-
-    console.log(board);
     console.timeEnd('wayOfDoing2');
