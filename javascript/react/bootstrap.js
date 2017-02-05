7d8eba094cf0579266a8d0d93b05f78646865131
Roger Silva de Souza <rogersilvasouza@hotmail.com>
2017-02-04 22:10:43 -0200

    Organização das categorias do JS

---
+++ javascript/react/bootstrap.js
@@ -0,0 +1,10 @@
+// Install create-react-app global
+sudo npm install -g create-react-app
+
+// creact project react
+create-react-app project
+cd project
+
+// install and start project with dependencies
+npm install
+npm start
